9524a2616ca23ed36e164d8beab7bbf156028d11
HarolBarajas <fabianbap32@gmail.com>
2026-04-12 21:51:56 -0600

    Práctica 1

--- demux/TCPclient.py
+++ demux/TCPclient.py
@@ -1,10 +1,10 @@
 import socket
 
-HOST="192.168.56.2"
-PORT = 5000
+HOST="localhost"
+PORT = 5050
 BUFSIZE = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketCliente:
-    socketCliente.connect((HOST, PORT))
+    socketCliente.connect(("0.0.0.0", PORT))
     socketCliente.send(b"Oye te estoy hablando server!\n Hazme caso")
     socketCliente.recv(BUFSIZE)
--- demux/TCPdemux.py
+++ demux/TCPdemux.py
@@ -1,15 +1,22 @@
 import socket
 
-HOST="192.168.56.1"
-PORT=5000
+HOST="localhost"
+PORT=5050
 BUFFER_SIZE=1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketServer:
-    print(f"Servidor listo y escuchando")
     socketServer.bind((HOST, PORT))
-    socketServer.listen(1)
+    socketServer.listen(5)
+    print(f"Servidor listo y escuchando")
     while True:
         conn, addr = socketServer.accept()
-        while True:
         with conn:
-            conn.sendall("Bienvenido al servidor".encode())
+            print(f"Cliente conectado: {addr}")
+            while True:
+                print("Esperando a recibir datos...")
+                data = conn.recv(BUFFER_SIZE)
+                print (f"Recibien: {data}")
+                if not data:
+                    break
+                print("Enviando respuesta a ", addr)
+                conn.sendall(data)
